94ab6c4a79e967e00acffaa00dfbd1f18a80ce69
ph <pedro.xit.fluxo.ff@gmail.com>
2025-07-23 09:20:06 -0300

    Adicionar

--- equipe2.js
+++ equipe2.js
@@ -4,3 +4,69 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+ 
+let estoques  = []
+
+function menu() {
+    console.log('<<<<<<ESTOQUE>>>>>>');
+    console.log('1. Adicionar o produto ao estoque');
+    console.log('2. Listar produtos do estoque');
+    console.log('3. Atualizar quantidade de produtos no estoque');
+    console.log('4. Revomer produto do estoque');
+    console.log('5. Verificar quantidade baixa de produtos');
+    console.log('6. Sair');
+    console.log('\n' + '='.repeat(30));
+
+    rl.question('Escolha uma opção: ', (opcao) => {
+        switch (opcao) {
+            case '1':
+                adicionarProduto();
+                break;
+            case '2':
+                ListarProdutosEmEstoque();
+                break;
+            case '3':
+                AtualizarAQuantidadeDoProduto();
+                break;
+            case '4':
+                removerProdutos();
+                break;
+            case '5':
+                verificarFalta();
+                break;
+            case '6':
+                rl.close();
+                console.log('Obrigado por usar o programa de Estoque. Até mais!!');
+                break;
+            default:
+                console.log('Opção inválida. Tente novamente.')
+                menu()
+        }
+    })
+}
+
+function adicionarProduto () {
+rl.question('Digite o nome do produto: ', (nome) => {
+    rl.question('Digite categoria do produto, (ex: Elétrodomesticos, calçados, masculino...etc): ', (categoria) => {
+        rl.question('Digite a quantidade de produtos: ', (quantidade) => {
+            rl.question('Digite o valor do produto: ', (valor) => {
+                const estoque = {
+                    nome,
+                    categoria,
+                    quantidade,
+                    valor,
+                };
+                estoques.push(estoque);
+                console.log('Seu produto foi adicionado com sucesso!!');
+                console.log('Deseja adicionar outro produto?: (s/n)');
+      
+                rl.question('', (resposta) => {
+                    resposta.toLowerCase() === 's'
+                        ? adicionarProduto()
+                        : menu();
+                    });
+                });
+            });
+        });
+    });
+}
